ea945242141ec1bdb04809b38d9711c10ccbf459
Unknown <cclozanoj@unal.edu.co>
2017-12-14 09:54:13 -0500

    triqui V1.0
    Still missess rematch, needs upgrade in logic-visual interaction

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -14,8 +14,10 @@ export class GameComponent implements OnInit {
 
 
   public saveNames(){
-    this.player1 = document.getElementById("In1").value;
-    this.player2 = document.getElementById("In2").value;
+    let x = document.getElementById("In1") as HTMLInputElement;
+    this.player1 = x.value;
+    let y = document.getElementById("In2") as HTMLInputElement;
+    this.player2 = y.value;
   }
 
   public default(){
@@ -24,7 +26,7 @@ export class GameComponent implements OnInit {
 
   public display() {
     var x = document.getElementById("first");
-    var y = document.getElementById("second"):
+    var y = document.getElementById("second");
     if (x.style.display === "none") {
       x.style.display = "block";
       if (y.style.display === "none") {
@@ -47,113 +49,203 @@ export class GameComponent implements OnInit {
   public move(id) {
     var top = window.getComputedStyle(document.getElementById(id)).getPropertyValue('top');
     var left = window.getComputedStyle(document.getElementById(id)).getPropertyValue('left');
-    document.getElementById(turno).style.top=top;
-    document.getElementById(turno).style.left=left;
-    console.log(turno + " " + id);
-    if ((turno % 2) == 0) {
-      positions[id.slice(0,-1)-1] = 1;
+    document.getElementById(this.turno).style.top=top;
+    document.getElementById(this.turno).style.left=left;
+    console.log(this.turno + " " + id);
+    if ((this.turno % 2) == 0) {
+      this.positions[id.slice(0,-1)-1] = 1;
     } else {
-      positions[id.slice(0,-1)-1] = 0;
+      this.positions[id.slice(0,-1)-1] = 0;
     }
-    console.log(positions);
-    turno += 1;
-    if(turno == 10){
-      turno =1;
+    console.log(this.positions);
+    this.turno += 1;
+    if(this.turno == 10){
+      this.turno =1;
     }
-    if(positions[0] == positions[1] && positions[1] == positions[2] && positions[0] == positions[2] && positions[0] != '-1' && positions[1] != '-1' && positions[2] != '-1'){
-      if ((turno % 2) == 0){
+    if(this.positions[0] == this.positions[1] && this.positions[1] == this.positions[2] && this.positions[0] == this.positions[2] && this.positions[0] != '-1' && this.positions[1] != '-1' && this.positions[2] != '-1'){
+      if ((this.turno % 2) == 0){
         this.punt1 += 1;
-        ganador = 1;
+        this.ganador = 1;
       } else {
         this.punt2 += 1;
-        ganador = 1;
+        this.ganador = 1;
       }
-      positions[0] = positions[1] = positions[2] = -1;
-    } else if (positions[3] == positions[4] && positions[4] == positions[5] && positions[3] == positions[5]  && positions[3] != '-1' && positions[4] != '-1' && positions[5] != '-1'){
-      if ((turno % 2) == 0){
+      this.positions[0] = this.positions[1] = this.positions[2] = -1;
+    } else if (this.positions[3] == this.positions[4] && this.positions[4] == this.positions[5] && this.positions[3] == this.positions[5]  && this.positions[3] != '-1' && this.positions[4] != '-1' && this.positions[5] != '-1'){
+      if ((this.turno % 2) == 0){
         this.punt1 += 1;
-        ganador = 1;
+        this.ganador = 1;
       } else {
         this.punt2 += 1;
-        ganador = 1;
+        this.ganador = 1;
       }
-      positions[3] = positions[4] = positions[5] = -1;
-    } else if (positions[6] == positions[7] && positions[7] == positions[8] && positions[6] == positions[7]  && positions[6] != '-1' && positions[7] != '-1' && positions[8] != '-1'){
-      if ((turno % 2) == 0){
+      this.positions[3] = this.positions[4] = this.positions[5] = -1;
+    } else if (this.positions[6] == this.positions[7] && this.positions[7] == this.positions[8] && this.positions[6] == this.positions[7]  && this.positions[6] != '-1' && this.positions[7] != '-1' && this.positions[8] != '-1'){
+      if ((this.turno % 2) == 0){
         this.punt1 += 1;
-        ganador = 1;
+        this.ganador = 1;
       } else {
         this.punt2 += 1;
-        ganador = 1;
+        this.ganador = 1;
       }
-      positions[6] = positions[7] = positions[8] = -1;
-    }else if (positions[0] == positions[3] && positions[3] == positions[6] && positions[10 == positions[6]  && positions[0] != '-1' && positions[3] != '-1' && positions[6] != '-1'){
-      if ((turno % 2) == 0){
+      this.positions[6] = this.positions[7] = this.positions[8] = -1;
+    }else if (this.positions[0] == this.positions[3] && this.positions[3] == this.positions[6] && this.positions[0] == this.positions[6]  && this.positions[0] != '-1' && this.positions[3] != '-1' && this.positions[6] != '-1'){
+      if ((this.turno % 2) == 0){
         this.punt1 += 1;
-        ganador = 1;
+        this.ganador = 1;
       } else {
         this.punt2 += 1;
-        ganador = 1;
+        this.ganador = 1;
       }
-      positions[0] = positions[3] = positions[6] = -1;
-    } else if (positions[1] == positions[4] && positions[4] == positions[7] && positions[1] == positions[7]  && positions[1] != '-1' && positions[4] != '-1' && positions[7] != '-1'){
-      if ((turno % 2) == 0){
+      this.positions[0] = this.positions[3] = this.positions[6] = -1;
+    } else if (this.positions[1] == this.positions[4] && this.positions[4] == this.positions[7] && this.positions[1] == this.positions[7]  && this.positions[1] != '-1' && this.positions[4] != '-1' && this.positions[7] != '-1'){
+      if ((this.turno % 2) == 0){
         this.punt1 += 1;
-        ganador = 1;
+        this.ganador = 1;
       } else {
         this.punt2 += 1;
-        ganador = 1;
+        this.ganador = 1;
       }
-      positions[1] = positions[4] = positions[7] = -1;
-    } else if (positions[2] == positions[5] && positions[5] == positions[8] && positions[2] == positions[8]  && positions[2] != '-1' && positions[5] != '-1' && positions[8] != '-1'){
-      if ((turno % 2) == 0){
+      this.positions[1] = this.positions[4] = this.positions[7] = -1;
+    } else if (this.positions[2] == this.positions[5] && this.positions[5] == this.positions[8] && this.positions[2] == this.positions[8]  && this.positions[2] != '-1' && this.positions[5] != '-1' && this.positions[8] != '-1'){
+      if ((this.turno % 2) == 0){
         this.punt1 += 1;
-        ganador = 1;
+        this.ganador = 1;
       } else {
         this.punt2 += 1;
-        ganador = 1;
+        this.ganador = 1;
       }
-      positions[2] = positions[5] = positions[8] = -1;
-    } else if (positions[0] == positions[4] && positions[4] == positions[8] && positions[0] == positions[8]  && positions[0] != '-1' && positions[4] != '-1' && positions[8] != '-1'){
-      if ((turno % 2) == 0){
+      this.positions[2] = this.positions[5] = this.positions[8] = -1;
+    } else if (this.positions[0] == this.positions[4] && this.positions[4] == this.positions[8] && this.positions[0] == this.positions[8]  && this.positions[0] != '-1' && this.positions[4] != '-1' && this.positions[8] != '-1'){
+      if ((this.turno % 2) == 0){
         this.punt1 += 1;
-        ganador = 1;
+        this.ganador = 1;
       } else {
         this.punt2 += 1;
-        ganador = 1;
+        this.ganador = 1;
       }
-      positions[0] = positions[4] = positions[8] = -1;
-    } else if (positions[6] == positions[4] && positions[4] == positions[2] && positions[6] == positions[2]  && positions[6] != '-1' && positions[4] != '-1' && positions[2] != '-1'){
-      if ((turno % 2) == 0){
+      this.positions[0] = this.positions[4] = this.positions[8] = -1;
+    } else if (this.positions[6] == this.positions[4] && this.positions[4] == this.positions[2] && this.positions[6] == this.positions[2]  && this.positions[6] != '-1' && this.positions[4] != '-1' && this.positions[2] != '-1'){
+      if ((this.turno % 2) == 0){
         this.punt1 += 1;
-        ganador = 1;
+        this.ganador = 1;
       } else {
         this.punt2 += 1;
-        ganador = 1;
+        this.ganador = 1;
       }
-      positions[2] = positions[4] = positions[6] = -1;
+      this.positions[2] = this.positions[4] = this.positions[6] = -1;
     }
-    if ( ganador == 1 ){
+    if ( this.ganador == 1 ){
       console.log("ganador");
-      document.getElementById("1b").disabled = true;
-      document.getElementById("2b").disabled = true;
-      document.getElementById("3b").disabled = true;
-      document.getElementById("4b").disabled = true;
-      document.getElementById("5b").disabled = true;
-      document.getElementById("6b").disabled = true;
-      document.getElementById("7b").disabled = true;
-      document.getElementById("8b").disabled = true;
-      document.getElementById("9b").disabled = true;
-      ganador = 0;
+      let x = document.getElementById("1b") as HTMLInputElement;
+      x.disabled = true;
+      x = document.getElementById("2b") as HTMLInputElement;
+      x.disabled = true;
+      x = document.getElementById("3b") as HTMLInputElement;
+      x.disabled = true;
+      x = document.getElementById("4b") as HTMLInputElement;
+      x.disabled = true;
+      x = document.getElementById("5b") as HTMLInputElement;
+      x.disabled = true;
+      x = document.getElementById("6b") as HTMLInputElement;
+      x.disabled = true;
+      x = document.getElementById("7b") as HTMLInputElement;
+      x.disabled = true;
+      x = document.getElementById("8b") as HTMLInputElement;
+      x.disabled = true;
+      x = document.getElementById("9b") as HTMLInputElement;
+      x.disabled = true;
+      this.ganador = 0;
     }
   }
-
-  punt1 = 0:
+  public rematch(){
+    let x = document.getElementById("1b") as HTMLInputElement;
+    x.disabled = false;
+    x = document.getElementById("2b") as HTMLInputElement;
+    x.disabled = false;
+    x = document.getElementById("3b") as HTMLInputElement;
+    x.disabled = false;
+    x = document.getElementById("4b") as HTMLInputElement;
+    x.disabled = false;
+    x = document.getElementById("5b") as HTMLInputElement;
+    x.disabled = false;
+    x = document.getElementById("6b") as HTMLInputElement;
+    x.disabled = false;
+    x = document.getElementById("7b") as HTMLInputElement;
+    x.disabled = false;
+    x = document.getElementById("8b") as HTMLInputElement;
+    x.disabled = false;
+    x = document.getElementById("9b") as HTMLInputElement;
+    x.disabled = false;
+    this.ganador = 0;
+    if ((this.partida % 2) == 1){
+      let x = document.getElementById("1") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      x = document.getElementById("2") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      x = document.getElementById("3") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      x = document.getElementById("4") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      x = document.getElementById("5") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      x = document.getElementById("6") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      x = document.getElementById("7") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      x = document.getElementById("8") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      x = document.getElementById("9") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      this.partida += 1;
+      this.turno = 0;
+    } else {
+      let x = document.getElementById("1") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      x = document.getElementById("2") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      x = document.getElementById("3") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      x = document.getElementById("4") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      x = document.getElementById("5") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      x = document.getElementById("6") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      x = document.getElementById("7") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      x = document.getElementById("8") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='20%';
+      x = document.getElementById("9") as HTMLInputElement;
+      x.style.top  ='40%';
+      x.style.left ='3%';
+      this.partida += 1;
+      this.turno = 1;
+    }
+  }
+  punt1 = 0;
   punt2 = 0;
-  var player1 = null;
-  var player2 = null;
-  var positions = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
-  var turno = 1;
-  var partida = 0;
-  var ganador = 0;
+  player1: String = null;
+  player2: String = null;
+  positions: any[] = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+  turno: any = 1;
+  partida: any = 1;
+  ganador: any = 0;
 }
